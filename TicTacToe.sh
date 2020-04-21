d9daebcb707a75c7fd6d8f5db33a5a31a8345882
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-04-21 15:17:34 +0530

    Add. Adding [UseCase3] to the branch checkToss.

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -9,11 +9,22 @@ echo "Game Started"
 }
 reset
 
+
 if [ $player -eq 1 ]
 then
 	symbol=o;
 else
 	symbol=x;
 fi
-
 echo "Player Symbol is" $symbol
+
+function checkToss(){
+toss=$(( RANDOM % 2))
+if [ $toss -eq 1 ]
+then
+	echo "Player Won Toss(Play first)"
+else
+	echo "Computers Won Toss(Play first)"
+fi
+}
+checkToss
